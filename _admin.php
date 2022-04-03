d659089cf2976314c6463f661ede551457478fe1
Philippe-dev <philippe@dissitou.org>
2022-04-03 13:09:43 +0200

    add dark mode icon

--- _admin.php
+++ _admin.php
@@ -1,28 +1,30 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-#
-# This file is part of Related, a plugin for DotClear2.
-#
-# Licensed under the GPL version 2.0 license.
-# See LICENSE file or
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
-# -- END LICENSE BLOCK ------------------------------------
+// -- BEGIN LICENSE BLOCK ----------------------------------
+//
+// This file is part of Related, a plugin for DotClear2.
+//
+// Licensed under the GPL version 2.0 license.
+// See LICENSE file or
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+//
+// -- END LICENSE BLOCK ------------------------------------
 
-if (!defined('DC_CONTEXT_ADMIN')) return;
+if (!defined('DC_CONTEXT_ADMIN')) {
+    return;
+}
 
 $_menu['Blog']->addItem(
-	__('Related pages'),
-	'plugin.php?p=related',
-	'index.php?pf=related/icon.svg',
-	preg_match('/plugin.php\?p=related(&.*)?$/',$_SERVER['REQUEST_URI']),
-	$core->auth->check('contentadmin,pages',$core->blog->id)
+    __('Related pages'),
+    $core->adminurl->get('admin.plugin.related'),
+    [dcPage::getPF('related/icon.svg'), dcPage::getPF('related/icon-dark.svg')],
+    preg_match('/' . preg_quote($core->adminurl->get('admin.plugin.related')) . '(&.*)?$/', $_SERVER['REQUEST_URI']),
+    $core->auth->check('contentadmin,pages', $core->blog->id)
 );
 
-$core->auth->setPermissionType('pages',__('manage related pages'));
+$core->auth->setPermissionType('pages', __('manage related pages'));
 
-$core->addBehavior('adminDashboardFavs', array('relatedAdminBehaviors', 'dashboardFavs'));
-$core->addBehavior('adminDashboardFavsIcon', array('relatedAdminBehaviors', 'dashboardFavsIcon'));
-$core->addBehavior('sitemapsDefineParts',array('relatedAdminBehaviors','sitemapsDefineParts'));
+$core->addBehavior('adminDashboardFavs', ['relatedAdminBehaviors', 'dashboardFavs']);
+$core->addBehavior('adminDashboardFavsIcon', ['relatedAdminBehaviors', 'dashboardFavsIcon']);
+$core->addBehavior('sitemapsDefineParts', ['relatedAdminBehaviors', 'sitemapsDefineParts']);
 
-require dirname(__FILE__).'/_widgets.php';
+require dirname(__FILE__) . '/_widgets.php';
