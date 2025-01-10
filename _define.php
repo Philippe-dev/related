c78e4708096412bf6b2d2a99e7c664c7485de2fa
“philippe <philippe.mail.perso@gmail.com>
2025-01-10 08:23:14 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -14,12 +14,12 @@
     'Related pages', // Name
     'Serve pages & scripts', // Description
     'Pep, Nicolas Roudaire and contributors', // Author
-    '2.4', // Version
+    '2.5', // Version
     [
         'permissions' => 'My',
         'type'        => 'plugin',
-        'dc_min'      => '2.32',
-        'requires'    => [['core', '2.32']],
+        'dc_min'      => '2.33',
+        'requires'    => [['core', '2.33']],
         'repository'  => 'https://github.com/Philippe-dev/related',
         'support'     => 'http://forum.dotclear.net/viewtopic.php?id=48205',
         'details'     => 'http://plugins.dotaddict.org/dc2/details/related',
