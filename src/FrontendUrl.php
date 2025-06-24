9c5e2fe8508cf2602a5764ef20c19e65cc4666ff
“philippe <philippe.mail.perso@gmail.com>
2025-06-24 10:03:39 +0200

    remove useless comments & trackbacks code

--- src/FrontendUrl.php
+++ src/FrontendUrl.php
@@ -48,20 +48,6 @@ class FrontendUrl extends Url
 
             App::frontend()->context()->posts = App::blog()->getPosts($params);
 
-            /**
-             * @var        ArrayObject<string, mixed>
-             */
-            $cp               = new ArrayObject();
-            $cp['content']    = '';
-            $cp['rawcontent'] = '';
-            $cp['name']       = '';
-            $cp['mail']       = '';
-            $cp['site']       = '';
-            $cp['preview']    = false;
-            $cp['remember']   = false;
-
-            App::frontend()->context()->comment_preview = $cp;
-
             App::blog()->withoutPassword(true);
 
             if (App::frontend()->context()->posts->isEmpty()) {
@@ -95,106 +81,7 @@ class FrontendUrl extends Url
                     }
                 }
 
-                $post_comment = isset($_POST['c_name']) && isset($_POST['c_mail']) && isset($_POST['c_site']) && isset($_POST['c_content']) && App::frontend()->context()->posts->commentsActive();
-
-                # Posting a comment
-                if ($post_comment) {
-                    # Spam trap
-                    if (!empty($_POST['f_mail'])) {
-                        Http::head(412, 'Precondition Failed');
-                        header('Content-Type: text/plain');
-                        echo 'So Long, and Thanks For All the Fish';
-                        # Exits immediately the application to preserve the server.
-                        exit;
-                    }
-
-                    $name    = $_POST['c_name'];
-                    $mail    = $_POST['c_mail'];
-                    $site    = $_POST['c_site'];
-                    $content = $_POST['c_content'];
-                    $preview = !empty($_POST['preview']);
-
-                    if ($content != '') {
-                        # --BEHAVIOR-- publicBeforeCommentTransform -- string
-                        $buffer = App::behavior()->callBehavior('publicBeforeCommentTransform', $content);
-                        if ($buffer !== '') {
-                            $content = $buffer;
-                        } else {
-                            if (App::blog()->settings()->system->wiki_comments) {
-                                App::filter()->initWikiComment();
-                            } else {
-                                App::filter()->initWikiSimpleComment();
-                            }
-                            $content = App::filter()->wikiTransform($content);
-                        }
-                        $content = App::filter()->HTMLfilter($content);
-                    }
-
-                    App::frontend()->context()->comment_preview['content']    = $content;
-                    App::frontend()->context()->comment_preview['rawcontent'] = $_POST['c_content'];
-                    App::frontend()->context()->comment_preview['name']       = $name;
-                    App::frontend()->context()->comment_preview['mail']       = $mail;
-                    App::frontend()->context()->comment_preview['site']       = $site;
-
-                    if ($preview) {
-                        # --BEHAVIOR-- publicBeforeCommentPreview -- ArrayObject
-                        App::behavior()->callBehavior('publicBeforeCommentPreview', App::frontend()->context()->comment_preview);
-
-                        # --BEHAVIOR-- coreContentFilter -- string, array<int, array<int, string>> -- since 2.34
-                        App::behavior()->callBehavior(
-                            'coreContentFilter',
-                            'comment',
-                            [
-                                [&$content, 'html'],
-                            ]
-                        );
-                        App::frontend()->context()->comment_preview['content'] = (string) $content;
-
-                        App::frontend()->context()->comment_preview['preview'] = true;
-                    } else {
-                        # Post the comment
-                        $cur = App::blog()->openCommentCursor();
-
-                        $cur->comment_author  = $name;
-                        $cur->comment_site    = Html::clean($site);
-                        $cur->comment_email   = Html::clean($mail);
-                        $cur->comment_content = $content;
-                        $cur->post_id         = App::frontend()->context()->posts->post_id;
-                        $cur->comment_status  = App::blog()->settings()->system->comments_pub ? App::status()->comment()::PUBLISHED : App::status()->comment()::PENDING;
-                        $cur->comment_ip      = Http::realIP();
-
-                        $redir = App::frontend()->context()->posts->getURL();
-                        $redir .= App::blog()->settings()->system->url_scan == 'query_string' ? '&' : '?';
-
-                        try {
-                            if (!Text::isEmail($cur->comment_email)) {
-                                throw new Exception(__('You must provide a valid email address.'));
-                            }
-
-                            # --BEHAVIOR-- publicBeforeCommentCreate -- Cursor
-                            App::behavior()->callBehavior('publicBeforeCommentCreate', $cur);
-                            if ($cur->post_id) {
-                                $comment_id = App::blog()->addComment($cur);
-
-                                # --BEHAVIOR-- publicAfterCommentCreate -- Cursor, int
-                                App::behavior()->callBehavior('publicAfterCommentCreate', $cur, $comment_id);
-                            }
-
-                            $redir_arg = App::status()->comment()->isRestricted($cur->comment_status) ? 'pub=0' : 'pub=1';
-
-                            header('Location: ' . $redir . $redir_arg);
-                        } catch (Exception $e) {
-                            App::frontend()->context()->form_error = $e->getMessage();
-                        }
-                    }
-                }
-
-                # The entry
-                if (App::frontend()->context()->posts->trackbacksActive()) {
-                    // Send additional headers if pingbacks/webmentions are allowed
-                    header('X-Pingback: ' . App::blog()->url() . App::url()->getURLFor('xmlrpc', App::blog()->id()));
-                    header('Link: <' . App::blog()->url() . App::url()->getURLFor('webmention') . '>; rel="webmention"');
-                }
+                
 
                 $tplset           = App::themes()->moduleInfo(App::blog()->settings()->system->theme, 'tplset');
                 $default_template = Path::real(App::plugins()->moduleInfo('related', 'root')) . DIRECTORY_SEPARATOR . Utility::TPL_ROOT . DIRECTORY_SEPARATOR;
@@ -231,7 +118,7 @@ class FrontendUrl extends Url
                     App::frontend()->context()->xframeoption = App::config()->adminUrl();
                 }
 
-                self::pages($post_url);
+                self::related($post_url);
             }
         }
     }
