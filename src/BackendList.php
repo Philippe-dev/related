83f5af03f00ae1ed356825a75038dc122ea50605
“philippe <philippe.mail.perso@gmail.com>
2025-06-27 17:40:35 +0200

    add attachments legend

--- src/BackendList.php
+++ src/BackendList.php
@@ -153,7 +153,8 @@ class BackendList extends Listing
                             $fmt(__('Scheduled'), 'scheduled.svg', 'scheduled') . ' - ' .
                             $fmt(__('Pending'), 'pending.svg', 'pending') . ' - ' .
                             $fmt(__('Protected'), 'locker.svg', 'locked') . ' - ' .
-                            $fmt(__('In widget'), 'selected.svg', 'selected')
+                            $fmt(__('In widget'), 'selected.svg', 'selected') . ' - ' .
+                            $fmt(__('Attachments'), 'attach.svg', 'attach')
                         )),
                     ]),
             ])
