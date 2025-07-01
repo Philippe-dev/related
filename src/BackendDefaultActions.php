c9da966b0a09490b7cf622f1460188e39f6849dd
“philippe <philippe.mail.perso@gmail.com>
2025-07-01 11:23:08 +0200

    Update BackendDefaultActions.php

--- src/BackendDefaultActions.php
+++ src/BackendDefaultActions.php
@@ -13,28 +13,8 @@ declare(strict_types=1);
 
 namespace Dotclear\Plugin\related;
 
-use ArrayObject;
 use Dotclear\App;
-use Dotclear\Core\Backend\Action\ActionsPosts;
 use Dotclear\Core\Backend\Action\ActionsPostsDefault;
-use Dotclear\Core\Backend\Combos;
-use Dotclear\Core\Backend\Notices;
-use Dotclear\Core\Backend\Page;
-use Dotclear\Database\Statement\UpdateStatement;
-use Dotclear\Helper\Html\Form\Div;
-use Dotclear\Helper\Html\Form\Form;
-use Dotclear\Helper\Html\Form\Hidden;
-use Dotclear\Helper\Html\Form\Input;
-use Dotclear\Helper\Html\Form\Label;
-use Dotclear\Helper\Html\Form\Link;
-use Dotclear\Helper\Html\Form\Para;
-use Dotclear\Helper\Html\Form\Select;
-use Dotclear\Helper\Html\Form\Submit;
-use Dotclear\Helper\Html\Form\Text;
-use Dotclear\Helper\Html\Html;
-use Dotclear\Helper\L10n;
-use Dotclear\Schema\Extension\User;
-use Exception;
 
 /**
  * @brief   The module backend default pages actions.
@@ -81,56 +61,5 @@ class BackendDefaultActions
                 ActionsPostsDefault::doDeletePost(...)
             );
         }
-
-        $ap->addAction(
-            [__('Order') => [
-                __('Save order') => 'reorder', ]],
-            self::doReorderPages(...)
-        );
     }
-
-    /**
-     * Does reorder pages.
-     *
-     * @param   BackendActions                  $ap     Admin actions instance
-     * @param   ArrayObject<string, mixed>      $post   The post
-     *
-     * @throws  Exception   If user permission not granted
-     */
-    public static function doReorderPages(BackendActions $ap, ArrayObject $post): void
-    {
-        foreach ($post['order'] as $post_id => $value) {
-            if (!App::auth()->check(App::auth()->makePermissions([
-                App::auth()::PERMISSION_PUBLISH,
-                App::auth()::PERMISSION_CONTENT_ADMIN,
-            ]), App::blog()->id())) {
-                throw new Exception(__('You are not allowed to change this entry status'));
-            }
-
-            $cur                = App::blog()->openPostCursor();
-            $cur->post_position = (int) $value - 1;
-            $cur->post_upddt    = date('Y-m-d H:i:s');
-
-            $sql = new UpdateStatement();
-            $sql
-                ->where('blog_id = ' . $sql->quote(App::blog()->id()))
-                ->and('post_id ' . $sql->in($post_id));
-
-            #If user can only publish, we need to check the post's owner
-            if (!App::auth()->check(App::auth()->makePermissions([
-                App::auth()::PERMISSION_CONTENT_ADMIN,
-            ]), App::blog()->id())) {
-                $sql->and('user_id = ' . $sql->quote((string) App::auth()->userID()));
-            }
-
-            $sql->update($cur);
-
-            App::blog()->triggerBlog();
-        }
-
-        Notices::addSuccessNotice(__('Pages have been successfully reordered.'));
-        $ap->redirect(false);
-    }
-
-    
 }
