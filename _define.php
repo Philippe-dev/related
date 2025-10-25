647c455bd406b89786b1f396a57f7b4e4510697f
“philippe <philippe.mail.perso@gmail.com>
2025-10-25 09:21:32 +0200

    Update _define.php

--- _define.php
+++ _define.php
@@ -13,9 +13,9 @@ $this->registerModule(
     'Included pages',
     'Serve HTML templates & PHP scripts',
     'Pep, Nicolas Roudaire and contributors',
-    '5.1',
+    '5.2',
     [
-        'date'        => '2025-10-24T12:12:00+0100',
+        'date'        => '2025-10-25T12:12:00+0100',
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
