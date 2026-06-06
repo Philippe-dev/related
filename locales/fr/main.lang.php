8883d7ebd934ce0a687d374f2f8e63ba0ae0fc40
philippe <philippe.mail.perso@gmail.com>
2026-06-06 12:09:57 +0200

    translations update

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+// Language: Français
+// Module: related - 7.4
+// Date: 2026-06-06 10:09:19
+// Translated with Translater - 2025.09.11
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['%d included pages'] = '%d pages incluses';
+L10n::$locales['%d included page'] = '%d page incluse';
+L10n::$locales['In widget:'] = 'Dans le widget :';
+L10n::$locales['Included pages'] = 'Pages incluses';
+L10n::$locales['Included pages actions'] = 'Actions sur le pages incluses';
+L10n::$locales['Add to widget'] = 'Ajouter au widget';
+L10n::$locales['Remove from widget'] = 'Enlever du widget';
+L10n::$locales['Pages have been successfully reordered.'] = 'Les pages ont été ordonnées avec succès.';
+L10n::$locales['In widget'] = 'Dans le widget';
+L10n::$locales['Directory "%s" for related files repository needs to allow read and write access.'] = 'Le répertoire "%s" pour le dépôt des fichiers liés nécessite un accès en lecture et écriture.';
+L10n::$locales['Configuration has been updated.'] = 'La configuration a été mise à jour.';
+L10n::$locales['Enable Related plugin'] = 'Activer le plugin pages incluses';
+L10n::$locales['General options'] = 'Options générales';
+L10n::$locales['Included files repository path:'] = 'Chemin du dépôt des fichiers inclus :';
+L10n::$locales['Advanced options'] = 'Options avancées';
+L10n::$locales['URL prefix:'] = 'Préfixe d\'URL :';
+L10n::$locales['This prefix will be used to generate the pages URLs. Do not use any existing prefix such as post, category or page.'] = 'Ce préfixe sera utilisé pour générer les URLs des pages. N\'utilisez pas un préfixe existant tel que post, category ou page.';
+L10n::$locales['Directory for related files repository needs to allow read and write access.'] = 'Le répertoire des pages incluses doit être accessible en lecture et écriture.';
+L10n::$locales['New included page'] = 'Nouvelle page incluse';
+L10n::$locales['Page has been updated.'] = 'La page a été mise à jour.';
+L10n::$locales['Page has been created.'] = 'La page a été créée.';
+L10n::$locales['Included file:'] = 'Fichier inclus :';
+L10n::$locales['Pick up a file in your repository:'] = 'Choisir un fichier dans votre dépôt :';
+L10n::$locales['Pick up a local file:'] = 'Choisir un fichier local :';
+L10n::$locales['Allowed file extensions : *.php, *.html, *.txt, *.xml'] = 'Extensions de fichiers autorisées : *.php, *.html, *.txt, *.xml';
+L10n::$locales['Serve HTML templates and PHP scripts'] = 'Inclure des templates HTML et des scripts PHP';
+L10n::$locales['Pages limit:'] = 'Limite du nombre de pages :';
+L10n::$locales['Pick up a local file in your included pages repository'] = 'Choisissez un fichier local dans votre dépôt de pages incluses';
+L10n::$locales['%d entry has been successfully added to the widget.'] = [
+	'%d page a été ajoutée au widget avec succès.',
+	'%d pages ont été ajoutées au widget avec succès.',
+];
+L10n::$locales['%d entry has been successfully removed from the widget.'] = [
+	'%d page a été enlevée du widget avec succès.',
+	'%d pages ont été enlevées du widget avec succès.',
+];
