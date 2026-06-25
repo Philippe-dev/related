0caaaeffa69de446244b89777ab2e1401831d832
philippe <philippe.mail.perso@gmail.com>
2026-06-25 06:26:29 +0200

    legacy page does not need this js

--- js/page.js
+++ js/page.js
@@ -1,16 +1,18 @@
 'use strict';
 
 dotclear.ready(() => {
-  const up_file = document.getElementById('up_file');
-  const files_dir = document.getElementById('files_dir');
+  if (document.getElementById('up_file')) {
+    const up_file = document.getElementById('up_file');
+    const files_dir = document.getElementById('files_dir');
 
-  function updateRequiredFields() {
-    up_file.required = !files_dir.value;
-    files_dir.required = !up_file.value;
+    function updateRequiredFields() {
+      up_file.required = !files_dir.value;
+      files_dir.required = !up_file.value;
+    }
+
+    updateRequiredFields();
+
+    files_dir.addEventListener('change', updateRequiredFields);
+    up_file.addEventListener('change', updateRequiredFields);
   }
-
-  updateRequiredFields();
-
-  files_dir.addEventListener('change', updateRequiredFields);
-  up_file.addEventListener('change', updateRequiredFields);
 });
